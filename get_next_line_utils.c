1e9b78a669dd900beaf4d3af94e6e0657103ceb9
moel-yag <moel-yag@student.1337.ma>
2024-12-10 17:06:18 +0100

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -12,64 +12,68 @@
 
 #include "get_next_line.h"
 
-size_t    ft_strlen(const char *s)
-{
-    size_t    i;
-
-    i = 0;
-    while (s[i])
-        i++;
-    return (i);
-}
-
-void	ft_bzero(void *s, size_t n)
+size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
 	i = 0;
-	while (i < n)
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*dup;
+	size_t	len;
+
+	len = ft_strlen(s);
+	dup = malloc((len + 1) * sizeof(char));
+	if (!dup)
+		return (NULL);
+	ft_strlcpy(dup, s, len + 1);
+	return (dup);
+}
+
+char	*ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < size - 1 && src[i])
 	{
-		((char *)s)[i] = '\0';
+		dst[i] = src[i];
 		i++;
 	}
+	dst[i] = '\0';
+	return (dst);
 }
 
-void	*ft_calloc(size_t n, size_t size)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	void	*ptr;
+	size_t	len1;
+	size_t	len2;
+	char	*str;
 
-	ptr = malloc(n * size);
-	if (!ptr)
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	str = malloc((len1 + len2 + 1) * sizeof(char));
+	if (!str)
 		return (NULL);
-	ft_bzero(ptr, n * size);
-	return (ptr);
+	ft_strlcpy(str, s1, len1 + 1);
+	ft_strlcpy(str + len1, s2, len2 + 1);
+	return (str);
 }
 
-char    *ft_strjoin(char const *s1, char const *s2)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t    len1;
-    size_t    len2;
-    char    *str;
+	char	*sub;
 
-    len1 = ft_strlen(s1);
-    len2 = ft_strlen(s2);
-    str = malloc((len1 + len2 + 1) * sizeof(char));
-    if (!str)
-        return (NULL);
-    ft_strlcpy(str, s1, len1 + 1);
-    ft_strlcpy(str + len1, s2, len2 + 1);
-    return (str);
-}
-
-char    *ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char    *sub;
-
-    if (!s)
-        return (NULL);
-    sub = malloc((len + 1) * sizeof(char));
-    if (!sub)
-        return (NULL);
-    ft_strlcpy(sub, s + start, len + 1);
-    return (sub);
+	if (!s)
+		return (NULL);
+	sub = malloc((len + 1) * sizeof(char));
+	if (!sub)
+		return (NULL);
+	ft_strlcpy(sub, s + start, len + 1);
+	return (sub);
 }
