515d88321ce1d08306b80eacf0abc0b78cf2623e
moel-yag <moel-yag@student.1337.ma>
2024-12-10 17:07:14 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -17,12 +17,13 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-# define BUFFER_SIZE 32
+# ifndef GET_NEXT_LINE
+#  define BUFFER_SIZE 5
+# endif
 
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
-void    ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t n, size_t size);
+char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strchr(const char *s, int c);
